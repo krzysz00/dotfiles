edcd6c2b2746efdc4b859217cbe695a0a51c69b5
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2019-09-19 22:29:05 -0700

    Get multi-dock working

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicBars
 import XMonad.Util.EZConfig
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
@@ -8,10 +9,11 @@ import XMonad.Actions.Minimize
 import XMonad.Layout.Minimize
 import XMonad.Hooks.Minimize
 import XMonad.Util.SpawnOnce
+import XMonad.Util.Run (spawnPipe)
 import XMonad.Config.Gnome (gnomeRegister)
 
 main =
-  xmonad =<< statusBar "xmobar" myPP toggleStrutsKey (ewmh defaults)
+  xmonad $ ewmh defaults
 
 myPP = xmobarPP { ppCurrent = \x -> "<fc=green>" ++ x ++ "</fc>"
                 , ppHidden = id
@@ -22,19 +24,36 @@ myPP = xmobarPP { ppCurrent = \x -> "<fc=green>" ++ x ++ "</fc>"
                 , ppOrder = \(w:_:t:_) -> [w,t]
                 }
 
+myPPInactive = myPP { ppCurrent = \x -> "<fc=cyan>" ++ x ++ "</fc>"
+                    }
+
 toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
 
 myManageHook = composeAll [
   isFullscreen --> doFullFloat,
   className =? "VirtualBox" --> doFloat]
 
+xmobarCreate :: DynamicStatusBar
+xmobarCreate (S sid) = spawnPipe $ "xmobar --screen " ++ show sid
+
+xmobarDestroy :: DynamicStatusBarCleanup
+xmobarDestroy = return ()
+
 defaults =
   def { modMask = mod4Mask
       , terminal = "gnome-terminal"
-      , manageHook = myManageHook <+> manageHook def
-      , layoutHook = minimize (Tall 1 (3/100) (1/2)) ||| layoutHook def
-      , handleEventHook = minimizeEventHook <+> handleEventHook def <+> fullscreenEventHook
-      , startupHook = gnomeRegister >> spawnOnce "~/.xmonad/xmonad-startup.sh"
+      , manageHook = manageDocks <+> myManageHook <+> manageHook def
+      , layoutHook = avoidStruts (minimize (Tall 1 (3/100) (1/2)) ||| layoutHook def)
+      , logHook = do
+          logHook def
+          multiPP myPP myPPInactive
+      , handleEventHook =
+        docksEventHook <+>
+        dynStatusBarEventHook xmobarCreate xmobarDestroy <+>
+        minimizeEventHook <+> fullscreenEventHook <+> handleEventHook def
+      , startupHook = docksStartupHook
+                      >> gnomeRegister >> spawnOnce "~/.xmonad/xmonad-startup.sh"
+                      >> dynStatusBarStartup xmobarCreate xmobarDestroy
                       >> startupHook def }
   `additionalKeysP` [("M-C-S-q", spawn "systemctl suspend")
                     ,("M-C-q", spawn "dm-tool switch-to-greeter")
@@ -42,6 +61,7 @@ defaults =
                     ,("M-S-l", spawn "gnome-screensaver-command --lock")
                     ,("M-m", withFocused minimizeWindow)
                     ,("M-S-m", withLastMinimized maximizeWindowAndFocus)
+                    ,("M-b", sendMessage ToggleStruts)
                     ,("M-p", spawn "dmenu_run -fn Inconsolata-21:normal")
                     ,("<XF86MonBrightnessUp>", spawn "xbacklight -inc 10")
                     ,("<XF86MonBrightnessDown>", spawn "xbacklight -dec 10")
